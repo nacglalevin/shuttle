b6a793cc52e6b5d455320014cce680ce1953246f
Lalevin <zzlyxht@outlook.com>
2024-06-08 21:56:22 +0800

    更新 shuttle.py

--- core/shuttle.py
+++ core/shuttle.py
@@ -503,16 +503,16 @@ def websploit():
 
 def metasploit():
       print('\n###### Installing Metasploit')
-    os.system("apt update -y && apt upgrade -y")
-    os.system("apt install git wget")
-    os.system("wget -O {}/metasploit.sh https://raw.github.com/gushmazuko/metasploit_in_termux/master/metasploit.sh".format(homeDir))
-    oldpwd = os.getenv("PWD")
-    os.chdir(os.getenv("HOME"))
-    os.system("bash metasploit.sh")
-    os.chdir(oldpwd)
-    print('###### Done')
-    print("###### Type 'msfconsole' to start.")
-    backtomenu_option()
+      os.system("apt update -y && apt upgrade -y")
+      os.system("apt install git wget")
+      os.system("wget -O {}/metasploit.sh https://raw.github.com/gushmazuko/metasploit_in_termux/master/metasploit.sh".format(homeDir))
+      oldpwd = os.getenv("PWD")
+      os.chdir(os.getenv("HOME"))
+      os.system("bash metasploit.sh")
+      os.chdir(oldpwd)
+      print('###### Done')
+      print("###### Type 'msfconsole' to start.")
+       backtomenu_option()
 
 def commix():
         print('\n###### Installing Commix')
